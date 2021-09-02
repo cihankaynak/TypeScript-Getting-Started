3c926d98af6189d7dc174c3cb835d69a4bd3bafa
Cihan Kaynak <cihan.kaynak@cerner.com>
2021-09-02 11:35:30 -0500

    Completed module 4.

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,35 @@
 function startGame() {
   // starting a new game
 
-  let playerName: string = "Cihan";
+  let playerName: string | undefined = getInputValue("playername");
   logPlayer(playerName);
-
-  let messagesElement = document.getElementById("messages");
-  messagesElement!.innerText =
-    "Welcome to MultiMath! Starting a really cool new game...";
+  postScore(100, playerName);
+  postScore(-5, playerName);
 }
 
-function logPlayer(name: string) {
-  console.log(`New game is starting for the player: ${name}`);
+function logPlayer(name: string = "MultiMath Player"): void {
+  logMessage(`New game is starting for the player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+  const inputElement = <HTMLInputElement>document.getElementById(elementId);
+  return inputElement.value === "" ? undefined : inputElement.value;
+}
+
+function postScore(
+  score: number,
+  playerName: string = "MultiMath Player"
+): void {
+  let logger: (value: string) => void = score < 100 ? logError : logMessage;
+  let scoreElement = document.getElementById("postedScore");
+  scoreElement!.innerText = `${score} - ${playerName}`;
+  logger(`Score:${score}`);
+}
+
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+  console.error(err);
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
